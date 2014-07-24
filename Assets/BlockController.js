8811f4aef6d466e29f14d2eb255ccace2838aa52
YoshinoriMurata <yoshinori@murata-no-MacBook-Air.local>
2014-07-24 15:23:21 +0900

    test
    test

---
+++ Assets/BlockController.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnCollisionEnter(collision: Collision){
+	Destroy(gameObject);
+	
+	rigidbody.velocity = rigidbody.velocity.normalized * 10;
+	
+	if (Mathf.Abs(rigidbody.velocity.y) < 3){
+		rigidbody.velocity.y = rigidbody.velocity.y * 3;
+	}
+	
+	if (Mathf.Abs(rigidbody.velocity.x) < 3){
+		rigidbody.velocity.x = rigidbody.velocity.x * 3;
+	}
+}
